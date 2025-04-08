0795dc047234f2af762c12ee5d87e2a7b0aa89af
Aalify-X <dev.aalifyx.com>
2025-04-08 17:12:01 +0300

    Added firebase.js and updated base.htm

---
+++ static/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB6IA0OfgM6C_EPYUqg1FCV907Inx84jw8",
+  authDomain: "progrify-f09b4.firebaseapp.com",
+  projectId: "progrify-f09b4",
+  storageBucket: "progrify-f09b4.firebasestorage.app",
+  messagingSenderId: "912535152076",
+  appId: "1:912535152076:web:570548e742dabd805d2969"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
