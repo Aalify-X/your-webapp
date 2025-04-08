564389d7628ca31f1b76134060d243a0dcd621c2
Aalify-X <dev.aalifyx@gmail.com>
2025-04-08 17:54:52 +0300

    Update firebase.js

--- static/firebase.js
+++ static/firebase.js
@@ -1,17 +1,19 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyB6IA0OfgM6C_EPYUqg1FCV907Inx84jw8",
-  authDomain: "progrify-f09b4.firebaseapp.com",
-  projectId: "progrify-f09b4",
-  storageBucket: "progrify-f09b4.firebasestorage.app",
-  messagingSenderId: "912535152076",
-  appId: "1:912535152076:web:570548e742dabd805d2969"
-};
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyC4jmpXYuACe3KzZulDPIPHvVnNG4GWGfM",
+    authDomain: "progrify-5d8f9.firebaseapp.com",
+    projectId: "progrify-5d8f9",
+    storageBucket: "progrify-5d8f9.firebasestorage.app",
+    messagingSenderId: "993029193028",
+    appId: "1:993029193028:web:68f42d025213a98c676b11"
+  };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
