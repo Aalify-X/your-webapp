71d201cdffa5d6823352c4c5732a469bb05e78c9
Aalify-X <dev.aalifyx@gmail.com>
2025-04-09 13:34:04 +0300

    Add files via upload

---
+++ static/index.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC4jmpXYuACe3KzZulDPIPHvVnNG4GWGfM",
+  authDomain: "progrify-5d8f9.firebaseapp.com",
+  projectId: "progrify-5d8f9",
+  storageBucket: "progrify-5d8f9.appspot.com",
+  messagingSenderId: "993029193028",
+  appId: "1:993029193028:web:68f42d025213a98c676b11"
+};
+
+const app = initializeApp(firebaseConfig);
