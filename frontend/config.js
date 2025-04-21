6e020d8e2911d4e6a30cf070d0b1e61d6cf4bfc6
Aalify-X <dev.aalifyx@gmail.com>
2025-04-21 16:27:03 +0300

    Update config.js

--- frontend/config.js
+++ frontend/config.js
@@ -1,2 +1,2 @@
 // config.js
-const API_BASE_URL = "[https://your-backend-service.onrender.com](https://your-backend-service.onrender.com)";
+const API_BASE_URL = "[https://your-webapp.onrender.com](https://your-webapp.onrender.com)";
